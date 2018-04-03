90176f9617a76ed5904f7889f427892cb776cf38
Pranta Sarker <prantacse04@gmail.com>
2018-04-03 22:35:09 +0600

    Add files via upload

---
+++ unsaved/js_button.php
@@ -0,0 +1,8 @@
+<button style="color: black" type="button" onclick="goBack()"> <- Go Back</button>
+		
+		
+		
+	<script>
+	function goBack() {
+    window.history.back();
+}
---
+++ unsaved/tmp.php
@@ -0,0 +1,8 @@
+<?php
+
+//include slideshow.php to access the Send_Slideshow_Data function
+include "slideshow.php";
+
+Send_Slideshow_Data ();
+
+?>
