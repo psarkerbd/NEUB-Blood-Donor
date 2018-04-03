1a00dd0006431414d51c951abbd63898a0919970
Pranta Sarker <prantacse04@gmail.com>
2018-04-03 22:33:25 +0600

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,19 @@
+</div>
+
+<script>
+	
+	function myfunction()
+	{
+		alert("Team member:140203020004:Pranta Sarker , 140203020005:Topu Dash Roy");
+	}
+
+</script>
+
+	    <div id="footer">
+		<p style="color:green" onclick="document.getElementById('demo').innerHTML = myfunction()"> Copyright@ TP </p>
+		</div>
+	  </div>
+
+	</body>
+
+</html> 
---
+++ include/u_header.php
@@ -0,0 +1,70 @@
+ <!DOCTYPE html>
+<html>
+	<head>
+	  <title>NEUB BD</title>
+	  <link href="css/main.css" rel="stylesheet" type="text/css">
+	</head>
+	
+	
+	<?php include("include/config.php");
+require_once("auth.php");
+
+
+				$id=$_SESSION['SESS_MEMBER_ID'];
+				
+				$sql="select * from user where id='$id' ";
+				$res=mysql_query($sql);
+				$s_a=mysql_fetch_array($res);
+				
+				
+				?>
+
+	<body>
+	
+	
+
+	 <div id="container">
+	    
+		<div id="header"> 
+		
+		<a href="index.php">
+		
+		<img src="admin/images/image.jpg" alt="NEUB Blood Donor" title="NEUB Blood Donor" style="width:950px; height:100px; text-align: right;">
+		
+		</a>
+		
+		<div id="slogan"> A Community of Voluntary Blood Donors of Bangladesh </div> 
+		
+		</div>
+	    
+		<!-- Search button Code 
+		<div id="skip-menu"></div>
+    <div class="column-right">
+      <div class="box"> !--->
+        
+		<form action="search.php" method="post" style="margin-top: 40px; margin-left:670px; margin-bottom:20px; margin-right:-1px;">
+		<!---
+		<input type="text" name="search" placeholder="  Search for...." style="border-radius:4px;" autocomplete="on" required>
+		!-->
+			<select name="blood_group">
+							<option value="">Search Donors</option>
+								<option value="A+">A(+ve)</option>
+								<option value="A-">A(-ve)</option>
+								<option value="B+">B(+ve)</option>
+								<option value="B-">B(-ve)</option>
+								<option value="O+">O(+ve)</option>
+								<option value="O-">O(-ve)</option>
+								<option value="AB+">AB(+ve)</option>
+								<option value="AB-">AB(-ve)</option>
+							</select>
+							
+							
+						<span class="glyphicon glyphicon-search"></span>
+						
+						<input type="submit" name="search" value="Search"  title="Search" class="btn btn-primary" />
+		
+		</form>
+		
+		<!-- End of Search button Code !--->
+		<div id="navigation">
+		  <ul>
